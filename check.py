7f9d86f4481f9447a9453e6d8b23df516ecba5f3
H1dek1 <hidekitakayama@gmail.com>
2021-12-14 15:06:37 +0900

    commit

--- check.py
+++ check.py
@@ -2,6 +2,15 @@
 import gym
 import skeleton_swimmer_env
 
-env = gym.make('SkeletonSwimmer-v0', isRecord=True, swimmer_type=10, action_period=0.001, max_arm_length=1.5)
+env = gym.make(
+        'SkeletonSwimmer-v0',
+        isRecord=True,
+        swimmer_type=10,
+        action_interval=1.5,
+        max_arm_length=1.5,
+        reward_gain=1.0,
+        penalty_gain=1.0,
+        epsilon=0.0
+        )
 
 env.test()
