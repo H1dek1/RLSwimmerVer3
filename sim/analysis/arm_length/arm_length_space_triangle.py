1357367c91f6d550736a0c2b6d7bc3ca6fb028c7
Hideki Takayam <hidekitakayama19960921@gmail.com>
2022-02-04 21:53:35 +0900

    commit

--- sim/analysis/arm_length/arm_length_space_triangle.py
+++ sim/analysis/arm_length/arm_length_space_triangle.py
@@ -5,6 +5,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
+import matplotlib
+matplotlib.use('Agg')
 
 plt.rcParams['mathtext.fontset'] = 'cm'
 plt.rcParams['font.size'] = 10
@@ -17,7 +19,7 @@ def main():
     # intervals = np.arange(0.1, 1.0, 0.2)
     # lengths = np.arange(1.1, 2.0, 0.2)
     intervals = [0.1]
-    lengths = [1.3]
+    lengths = [1.1]
 
     for interval in tqdm(intervals):
         for length in tqdm(lengths, leave=False):
@@ -26,7 +28,7 @@ def main():
             #         f'_maxlength{length:.1f}_withoutEnergy.csv'
             filename = f'../../result/' \
                     f'type20_radius0.1_interval{interval:.1f}' \
-                    f'_maxlength{length:.1f}_withEnergy.csv'
+                    f'_maxlength{length:.1f}_withoutEnergy.csv'
 
             df = pd.read_csv(filename)
             fig = plt.figure()
@@ -44,8 +46,9 @@ def main():
             ax.plot3D(df['arm_length_0'][int(plot_start/dt):int(plot_end/dt)], df['arm_length_1'][int(plot_start/dt):int(plot_end/dt)], df['arm_length_2'][int(plot_start/dt):int(plot_end/dt)], color='C0', label=filename)
 
             fig.savefig(f'img/custom_gamma/triangle/radius0.1/interval{interval:.1f}_length{length:.1f}.png')
-            del fig
+            fig.savefig('sample.png')
             plt.show()
+            del fig
 
 if __name__ == '__main__':
     main()
