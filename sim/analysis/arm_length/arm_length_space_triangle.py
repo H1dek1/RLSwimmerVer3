9cf9f6c1edf7dc2a2f243365d847232dca183e95
Hideki Takayam <hidekitakayama19960921@gmail.com>
2022-02-04 22:40:47 +0900

    commit

--- sim/analysis/arm_length/arm_length_space_triangle.py
+++ sim/analysis/arm_length/arm_length_space_triangle.py
@@ -6,20 +6,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 import matplotlib
-matplotlib.use('Agg')
 
 plt.rcParams['mathtext.fontset'] = 'cm'
 plt.rcParams['font.size'] = 10
 
 def main():
-    plot_start = 10
-    plot_end   = 50
+    plot_start = 500
+    plot_end   = 1000
     dt = 0.1
 
     # intervals = np.arange(0.1, 1.0, 0.2)
     # lengths = np.arange(1.1, 2.0, 0.2)
-    intervals = [0.1]
-    lengths = [1.1]
+    intervals = [0.7]
+    lengths   = [1.7]
 
     for interval in tqdm(intervals):
         for length in tqdm(lengths, leave=False):
@@ -38,9 +37,9 @@ def main():
             ax.set_xlabel(r'$\ell_0^*$', fontsize=20)
             ax.set_ylabel(r'$\ell_1^*$', fontsize=20)
             ax.set_zlabel(r'$\ell_2^*$', fontsize=20)
-            # ax.set_xlim(1, 2)
-            # ax.set_ylim(1, 2)
-            # ax.set_zlim(1, 2)
+            ax.set_xlim(1, 2)
+            ax.set_ylim(1, 2)
+            ax.set_zlim(1, 2)
             ax.view_init(elev=30, azim=10)
 
             ax.plot3D(df['arm_length_0'][int(plot_start/dt):int(plot_end/dt)], df['arm_length_1'][int(plot_start/dt):int(plot_end/dt)], df['arm_length_2'][int(plot_start/dt):int(plot_end/dt)], color='C0', label=filename)
