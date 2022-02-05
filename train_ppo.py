71697450e77354ff2d61a937839a5ce735328648
Hideki Takayama <hidekitakayama19960921@gmail.com>
2022-02-05 22:15:34 +0900

    commit

--- train_ppo.py
+++ train_ppo.py
@@ -21,8 +21,8 @@ def main():
     params = {
             'swimmer_type':       20,
             'on_record':          False,
-            'action_interval':    0.5,
-            'max_length':         1.9,
+            'action_interval':    0.7,
+            'max_length':         1.5,
             'consider_energy':    False,
             'random_init_states': False
             }
